d485d800629733f14576e1552e1ba7e2fe3290a8
An Nguyen <an310898@gmail.com>
2022-02-18 12:55:29 +0700

    An push RoomSettimg

---
+++ src/models/RoomSettingModel.java
@@ -0,0 +1,87 @@
+package models;
+
+public class RoomSettingModel {
+     String number;
+     String type;
+     String status;
+     int floor1;
+     String price;
+    String perHours;
+
+
+
+
+    public String getPerHours() {
+        return perHours;
+    }
+
+    public void setPerHours(String perHours) {
+        this.perHours = perHours;
+    }
+
+
+    public RoomSettingModel(String number, String type, String status, int floor1, String price, String perHours) {
+        this.number = number;
+        this.type = type;
+        this.status = status;
+        this.floor1 = floor1;
+        this.price = price;
+        this.perHours = perHours;
+    }
+
+    public String getPricePerHours() {
+        return pricePerHours;
+    }
+
+    public void setPricePerHours(String pricePerHours) {
+        this.pricePerHours = pricePerHours;
+    }
+
+    String pricePerHours;
+    public int getFloor1() {
+        return floor1;
+    }
+
+    public void setFloor1(int floor1) {
+        this.floor1 = floor1;
+    }
+
+
+
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+
+}
